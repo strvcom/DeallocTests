9859938801d0a0eb25b8983cf6f4d325f5c2dac7
Daniel Čech <daniel.cech@gmail.com>
2019-09-12 16:30:27 +0200

    [feat]: generated framework template

---
+++ Sources/DeallocTests.h
@@ -0,0 +1,17 @@
+//
+//  DeallocTests.h
+//  DeallocTests
+//
+//  Created by Daniel Cech on 01/04/19.
+//  Copyright © 2019 DanielCech. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DeallocTests.
+FOUNDATION_EXPORT double DeallocTestsVersionNumber;
+
+//! Project version string for DeallocTests.
+FOUNDATION_EXPORT const unsigned char DeallocTestsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DeallocTests/PublicHeader.h>
