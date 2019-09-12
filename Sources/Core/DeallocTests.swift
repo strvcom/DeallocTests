c2a7d57342a0b37601a672f1d7f0d561e548cb18
Daniel Čech <daniel.cech@gmail.com>
2019-09-12 16:26:43 +0200

    [feat]: basic framework template

---
+++ Sources/Core/DeallocTests.swift
@@ -0,0 +1,13 @@
+//
+//  DeallocTests.swift
+//  DeallocTests
+//
+//  Created by Daniel Cech on 01/04/19.
+//  Copyright © 2019 DanielCech. All rights reserved.
+//
+
+import Foundation
+
+class DeallocTests {
+    static let name = "DeallocTests"
+}
