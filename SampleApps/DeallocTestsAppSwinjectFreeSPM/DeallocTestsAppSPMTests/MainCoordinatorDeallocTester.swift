6700f3acf8f7f97dbd8f60be40c3600310cc8ab9
Daniel Cech <daniel.cech@strv.com>
2020-09-17 11:49:35 +0200

    [feat]: both libraries are working with cocoapods and spm

---
+++ SampleApps/DeallocTestsAppSwinjectFreeSPM/DeallocTestsAppSPMTests/MainCoordinatorDeallocTester.swift
@@ -0,0 +1,56 @@
+//
+//  MainCoordinatorDeallocTester.swift
+//  DeallocTestsAppTests
+//
+//  Created by Daniel Cech on 01/05/2020.
+//  Copyright © 2020 STRV. All rights reserved.
+//
+
+import Foundation
+import DeallocTestsSwinjectFree
+@testable import DeallocTestsAppSPM
+
+
+class MainCoordinatorDeallocTester: DeallocTester {
+    var mainCoordinator: MainCoordinator? {
+        applyAssembliesToContainer()
+
+        return MainCoordinator()
+    }
+
+    func test_mainCoordinatorDealloc() {
+        presentingController = showPresentingController()
+        
+        deallocTests = [
+            DeallocTest(
+                objectCreation: { [weak self] in
+                    return self?.mainCoordinator?.createFirstViewController()
+                }
+            ),
+            DeallocTest(
+                objectCreation: { [weak self] in
+                    return self?.mainCoordinator?.createSecondViewController()
+                }
+            ),
+            DeallocTest(
+                objectCreation: { [weak self] in
+                    return self?.mainCoordinator?.createThirdViewController()
+                }
+            ),
+            DeallocTest(
+                objectCreation: {
+                    return MainCoordinator()
+                }
+            )
+        ]
+
+        let expectation = self.expectation(description: "deallocTest test_mainCoordinatorDealloc")
+
+        performDeallocTest(
+            deallocTests: deallocTests,
+            expectation: expectation
+        )
+
+        waitForExpectations(timeout: 200, handler: nil)
+    }
+}
